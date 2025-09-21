5c3ac238dc4596957b54f065f2fcdc15885086c0
ASura-Shadow <gyan6320@gmail.com>
2025-09-22 02:45:03 +0530

    feat: close the sidebar on link click.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,13 @@
 const navbar = document.querySelector(".navbar");
 const hamburger = document.querySelector(".hamburger");
+const navLinks = [...document.querySelectorAll(".navbar a")];
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
 });
